64eb37f4bd2ebae075e61e920023c3b982750872
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-25 14:42:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,7 @@ def line(current_line)
   if current_line.size == 0
     puts "The line is currently empty."
   else
-
+    print_line = "The line is currently:"
+    current_line.each_with_index {|customer, index| print_line << " #{index+1}. #{customer}"}
   end
 end
