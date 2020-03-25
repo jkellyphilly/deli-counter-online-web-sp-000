68c6befa0ff0a8267b8f79918820739bd4b47395
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-25 14:47:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,11 @@ def take_a_number(current_line, new_customer)
   current_line << new_customer
   puts "Welcome, #{new_customer}. You are number #{current_line.size} in line."
 end
+
+def now_serving(current_line)
+  if current_line.size == 0
+    puts "There is nobody waiting to be served"
+  else
+
+  end
+end
